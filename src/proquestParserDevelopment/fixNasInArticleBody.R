5b295bcc8ee0a77a98e8a4f8c9844aaef1c1557e
eiriki <eiriki@vt.edu>
2018-06-26 14:20:24 +0000

    updating parse fix

--- src/proquestParserDevelopment/fixNasInArticleBody.R
+++ src/proquestParserDevelopment/fixNasInArticleBody.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(zoo)
 
 # These set the filename of the html file we're looking at, as well as the names of the fields we want to extract.
-proQuestHtml = "./data/business_innovation/original/scrapedProquestData/pfizer1.html"
+proQuestHtml = "./data/business_innovation/original/scrapedProquestData/novartis1.html"
 fieldsOfInterest = c("Subject", "Company", "Publication title", "Publication date", "Publication subject", "Source type", "Document type")
 
 # What follows are the guts of the parseProquest function. I've spilled them here so you can more easily tool around with them.
@@ -17,7 +17,7 @@ p = length(fieldsOfInterest)
 
 # Here, i sets the particular article you're looking at in the list of divs. I suggest looking at the first to see what it does, then go find some articles where the body text didn't parse and see if you can fix it.
 
-i = 1
+i = 127
 
 # Article title
 
@@ -41,6 +41,13 @@ for(j in 1:p){
 
 # Full text. Here's where the action is. Have fun.
 fullTextParagraphs = html_nodes(divs[i], "p") %>% html_attr("style") %>% is.na
+
+#simple conditional handling NA
+
+if(!all(is.na(fullTextParagraphs))){
+  print(TRUE)
+  }
+
 txt = paste((html_nodes(divs[i], "p") %>% html_text)[fullTextParagraphs], collapse = '')
 txt = gsub("\\n", "", txt) # This gets rid of newline characters
 txt = str_extract(txt, ".{1,5000}") #This regex selects the first 5000 characters. I was running into space limits in excel, so I did this.
