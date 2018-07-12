890457bc016593dfbdef5a5389a70e282de9e260
Raghav Sawhney <ragsaw96@vt.edu>
2018-07-12 13:13:50 +0000

    Committing final standardization code

--- src/nber_name_standardization_routine/Company_Name_Standardize_2.R
+++ src/nber_name_standardization_routine/Company_Name_Standardize_2.R
@@ -12,20 +12,32 @@ assignee <- read.dta("data/business_innovation/original/nber/assignee.dta")
 # Dynamic match of patent assignee to corporate entity
 dynass <- read.dta("data/business_innovation/original/nber/dynass.dta")
 
+
+# read in stata code files with information we can use
+derwent<-readLines("src/nber_name_standardization_routine/derwent_standardisation_BHH.do")
+derwent<-derwent[11:length(derwent)]
+
+str_extract_all(derwent[1], '(?<=\\")([^,]*?)(?=\\")')
+
+str_extract_all(derwent, '(?<=\\")([^,]*?)(?=\\")', simplify = TRUE) -> derwent_before_after
+
+head(derwent_before_after)
+
 # read in stata code files with information we can use
 name <-readLines("src/nber_name_standardization_routine/standard_name.do")
 name <-name[21:length(name)]
 
 str_extract_all(name[1], '(?<=\\")([^,]*?)(?=\\")')
 
-str_extract_all(name, '(?<=\\")([^,]*?)(?=\\")', simplify = TRUE) -> name_1
+str_extract_all(name, '(?<=\\")([^,]*?)(?=\\")', simplify = TRUE) -> name_before_after
 
-head(name_1)
+head(name_before_after)
+name_before_after[23]
+name_before_after = name_before_after[nchar(name_before_after[,1])>0,]
+head(name_before_after)
 
+all_before_after <- rbind(name_before_after,derwent_before_after)
 
-nameonly<-readLines("src/nber_name_standardization_routine/nameonly_main.do")
-nameonly<-nameonly[32:301]
-nameonly.df<-data.frame(nameonly)
-nameonly.df$nameonly<-as.character(nameonly.df$nameonly)
+colnames(all_before_after) = c("Before_String", "After_String")
 
-string<-nameonly.df$nameonly
+write.csv(all_before_after, "./data/business_innovation/working/Company_Standardization_Rules.csv", row.names = FALSE)
