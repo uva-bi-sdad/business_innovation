091d62ab28a39735d8f707992716cfcea1ade0d0
eiriki <eiriki@vt.edu>
2018-07-18 19:50:23 +0000

    str trim added

--- src/eiriki/naming.R
+++ src/eiriki/naming.R
@@ -6,6 +6,8 @@ library(stringr)
 FDA <- read_xlsx("./data/business_innovation/original/fda_drugs/Copy of FDA Database - COMBINED V2.xlsx")
 TJ <- read.csv('./data/business_innovation/working/PHARMACY_TIMES/combined/otc_dirty.csv',stringsAsFactors = F)
 
+#FDA <- read.csv('./data/business_innovation/working/med_device_data/masterkey.csv')
+#TJ <- read.csv('./data/business_innovation/working/Medical_Devices_Briefs/Standardized_Company_Names.csv')
 #uniques
 FDA_vec <- unique(FDA$Company)
 TJ_vec <- unique(TJ$Company)
@@ -14,21 +16,24 @@ TJ_vec <- unique(TJ$Company)
 FDA_vec <- tolower(FDA_vec)
 TJ_vec <- tolower(TJ_vec)
 #still cleaning
-FDA_vec <- str_replace_all(FDA_vec," inc$","") %>%
+FDA_vec <- str_trim(FDA_vec, side = 'both') %>%
+  str_replace_all("[[:punct:]]", "") %>%
+  str_replace_all(" inc$","") %>%
   str_replace_all(" pharm.*$","") %>%
   str_replace_all(" technologies$","") %>%
   str_replace_all(" intl$", "") %>%
   str_replace_all(" llc$", "") %>%
-  str_replace_all(" co$", "") %>%
-  str_replace_all("[[:punct:]]", "")
+  str_replace_all(" co$", "")
 #still cleaning
-TJ_vec <- str_replace_all(TJ_vec," inc$","") %>%
+TJ_vec <-  str_trim(TJ_vec, side = 'both') %>%
+  str_replace_all("[[:punct:]]", "") %>%
+  str_replace_all(" inc$","") %>%
   str_replace_all(" pharm.*$","") %>%
   str_replace_all(" technologies$","") %>%
   str_replace_all(" intl$", "") %>%
   str_replace_all(" llc$", "") %>%
-  str_replace_all(" co$", "") %>%
-  str_replace_all("[[:punct:]]", "")
+  str_replace_all(" co$", "")
+
 
 FDA_vec <- unique(FDA_vec)
 TJ_vec <- unique(TJ_vec)
@@ -51,10 +56,10 @@ y$std_dist <- 2*y$value / (nchar(y[,1]) + nchar(y[,2]))
 #graphics
 hist(y$std_dist)
 
-quantile(y$std_dist,seq(0,1,length = 10))
+quantile(y$std_dist,seq(0,1,length =11))
 
 y = y[order(y$std_dist),]
-
+View(head(y, 30))
 #saving out to file
 #write.csv(y, file= "./data/business_innovation/working/Name Standardization/FDADrugs_Pharmtimes_Companies.csv", row.names = F)
 
