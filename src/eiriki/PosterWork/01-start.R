ddf075b924d5829959b16b7648250a8fb9f3dbc3
eiriki <eiriki@vt.edu>
2018-07-25 18:34:49 +0000

    starting to get good stuff

--- src/eiriki/PosterWork/01-start.R
+++ src/eiriki/PosterWork/01-start.R
@@ -1,3 +1,26 @@
 # Exploratory work
 library(ggplot2)
 library(dplyr)
+library(stringr)
+source('./src/eiriki/PosterWork/cleaning.R')
+#only applying cleaning to Drugs journals
+pharmtimes <- read.csv('./data/business_innovation/working/PHARMACY_TIMES/combined/combined.csv')
+ptoday <- read.csv('./data/business_innovation/working/PHARMACY_TODAY/pToday.csv')
+
+co_name <- pharmtimes$Company
+co_name <- tolower(co_name)
+class(co_name)
+
+#run the cleaning function on our data, adjust as needed
+x <- cleaning(co_name)
+pharmtimes$Company <- x
+pharmtimes$Company <- as.factor(pharmtimes$Company)
+plot1 <- table(pharmtimes$Company)
+plot1 <- as.data.frame(plot1)
+colnames(plot1) <- c("Company", "Freq")
+plot2 <- subset(plot1, plot1$Freq > 10)
+ggplot(plot2, aes(x=Company, y = Freq)) + geom_col()
+
+
+
+
---
+++ src/eiriki/PosterWork/cleaning.R
@@ -0,0 +1,56 @@
+#function for quickly cleaning company names
+cleaning <- function(name_list){
+  clean_name <- str_trim(name_list, side = 'both') %>%
+    str_replace_all(" \\+ ", " and ") %>%
+    str_replace_all("\\,", "") %>%
+    # str_replace_all("[[:punct:]]", "") %>%
+    str_replace_all(" inc$","") %>%
+    str_replace_all(" pharm.*$","") %>%
+    str_replace_all(" technologies$","") %>%
+    str_replace_all(" intl$", "") %>%
+    str_replace_all(" llc$", "") %>%
+    str_replace_all(" co$", "") %>%
+    str_replace_all(" north america$", "") %>%
+    str_replace_all(" international$", "") %>%
+    str_replace_all(" labs$", "") %>%
+    # str_replace_all(" products$", "") %>%
+    str_replace_all("sanofiaventis", "sanofi aventis") %>%
+    str_replace_all(" us$", "") %>%
+    str_replace_all(" corp.*$", "") %>%
+    str_replace_all(" industries$", "") %>%
+    str_replace_all(" electronics$", "") %>%
+    str_replace_all(" medical$", "") %>%
+    str_replace_all(" cons ", " consumer ") %>%
+    str_replace_all(" cons$", " consumer")%>%
+    str_replace_all(" con ", " consumer ")%>%
+    str_replace_all(" con$", " consumer")%>%
+    str_replace_all("hlth", "health") %>%
+    str_replace_all("health care", "healthcare") %>%
+    str_replace_all("glaxosmithkline consumer [^h]", "glaxosmithkline consumer healthcare") %>%
+    str_replace_all("philadelphia pa", "") %>%
+    str_replace_all("philadelphia", "") %>%
+    str_replace_all("pittsburgh", "") %>%
+    str_replace_all("glaxosmithkline consumer [^h]", "glaxosmithkline consumer healthcare") %>%
+    str_replace_all("philadelphia", "") %>%
+    str_replace_all("philadelphia pa", "") %>%
+    str_replace_all(" pa(\\s|$)", "") %>%
+    str_replace_all(" england", "") %>%
+    str_replace_all(" grp ltd", " grp") %>%
+    str_replace_all(" denver", "") %>%
+    str_replace_all("(?= health(\\s?)care)(.*)", "") %>%
+    str_trim(side = 'both') %>%
+    str_replace_all(" consumer healthcare.*$", " consumer healthcare") %>%
+    str_replace_all(" ireland$", "") %>%
+    str_replace_all("johnsonmerck", "johnson-merck") %>%
+    str_replace_all("johnson johnson ", "johnson and johnson ") %>%
+    str_replace_all("johnson  johnson ", "johnson and johnson ") %>%
+    str_replace_all("j and j", "johnson and johnson") %>%
+    str_replace_all("johnson and johnson consumer$", "johnson and johnson consumer healthcare") %>%
+    str_replace_all("johnson and johnsonconsumer healthcare", "johnson and johnson consumer healthcare") %>%
+    str_replace_all("(?= health(\\s?)care)(.*)", " healthcare") %>%
+    str_replace_all(" and$", "")%>%
+    str_replace_all(" bristolmyers squibb", "bristol myers squibb") %>%
+    str_replace_all("sigmatau", "sigma tau") %>%
+    str_replace_all(" ip$", "")
+  return(clean_name)
+}
