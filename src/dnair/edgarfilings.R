2961b8c05cdbf065c0967d45bbc3e10180f49eb1
Devika Nair <dnair@virginia.edu>
2019-03-06 18:04:08 +0000

    slight changes to edgar script

--- src/dnair/edgarfilings.R
+++ src/dnair/edgarfilings.R
@@ -8,10 +8,11 @@ edgar3 <- xml2::read_xml(txtfile)
 edgar4
 class(edgar4)
 library(xml2)
+library(rvest)
 xml_name(edgar4) #html
 children <- xml_children(edgar4)
 
-baz <- xml_find_all(x, ".//baz")
+#baz <- xml_find_all(x, ".//baz")
 div <- xml_find_all(edgar4, ".//div")
 div_text <- html_text(div)
 div_text_clean <- stringr::str_squish(div_text)
@@ -27,15 +28,20 @@ word_ex2 <- stringr::str_split(div_text_clean_20plus[100], " ", simplify = FALSE
 word_ex2_caps <- grepl("^[[:upper:]]", unlist(word_ex2))
 word_ex2_eng <- hunspell_check(unlist(word_ex2), dict = dictionary("en_US"))
 
-example <- tibble("Word" = unlist(word_ex2),
+example <- tibble::tibble("Word" = unlist(word_ex2),
        "Capitals" = word_ex2_caps,
        "English" = word_ex2_eng)
 
-example %>% filter(English == FALSE)
+library(data.table)
+library(dplyr)
+example <- as.data.table(example) # %>% filter(English == FALSE)
+View(example)
 
-words_example <- as.data.frame(unlist(word_ex2))
-colnames(words_example) <- c("Word")
-words_example %>% mutate("English" = hunspell_check(words_example$Word, dict = dictionary("en_US")))
+# words_example <- as.data.frame(unlist(word_ex2))
+# colnames(words_example) <- c("Word")
+# words_example %>%
+#   mutate("English" = hunspell_check(as.character(words_example$Word), dict = dictionary("en_US"))) %>%
+#   as.data.table()
 
 ## another example
 row <- div_text_clean_20plus[200]
@@ -43,10 +49,35 @@ row_words <- unlist(stringr::str_split(gsub('[[:punct:] ]+',' ', row), " ", simp
 
 row_eng <- hunspell_check(row_words, dict = dictionary("en_US"))
 row_caps <- grepl("^[[:upper:]]", row_words)
+row_FDA <- grepl("FDA", row_words)
+word_innov<- c("launch", "new", "product", "FDA", "approval", "innovation", "innovative")
+row_launch <- grepl(paste(word_innov, collapse = "|"), row_words)
+#row_position <- row_words %>% mutate("Position" = row_number())
+library(stringdist)
+library(qdap)
 
-example2 <- tibble("Word" = row_words,
+library(devtools)
+install_github("trinker/qdapDictionaries")
+install_github("trinker/qdapRegex")
+install_github("trinker/qdapTools")
+install_github("trinker/qdap")
+install.packages("qdap", INSTALL_opts = "--no-multiarch")
+library()
+word_fda <- c("FDA", "approval")
+stringdist(word_innov, row_words, q = 1)
+
+# Raghav input on additional terms/proximities?
+# proximity - within 15 words
+
+example2 <- tibble::tibble("Word" = row_words,
                   "Capitals" = row_caps,
-                  "English" = row_eng)
+                  "English" = row_eng,
+                  "Launch" = row_launch,
+                  "Position" = row_position) %>%
+  as.data.table()
 
+View(example2)
 test <- example2 %>% filter(English == FALSE) %>% arrange(desc(Capitals))
-test[7,1]
+test[1:10,]
+#test[7,1]
+
