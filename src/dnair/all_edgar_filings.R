61e643ee68d692cff6bacbab1f486cb1db04ab72
Aaron D. Schroeder <aschroed@virginia.edu>
2019-03-13 17:31:48 +0000

    added comments

--- src/dnair/all_edgar_filings.R
+++ src/dnair/all_edgar_filings.R
@@ -49,6 +49,9 @@ removeDocTypes <-
 ## for loop
 if (exists("fin_o") == TRUE) rm(fin_o)
 
+# Loop over file paths. For safety, I specify subsets (e.g. file_names[1:1000]) and
+# then write each to a file, combining all files at the end
+# use trycatch to skip errors
 for (i in file_names[2002:2867]) {
   tryCatch({
     unclean <- read_file(i)
@@ -121,18 +124,18 @@ for (i in file_names[2002:2867]) {
     #   arrange(desc(count)) %>%
     #   data.table::as.data.table()
 
-    print(output1)
-    #print(output2)
+    #print(output1)
 
+    # Group by Company and Words and get count of records for each group. Then order by count descending.
     o1 <- setDT(output1)
     o2 <-
-      o1[English == FALSE &
-           Capitals == TRUE & nchar(Words) > 3 & substrRight(Words, 1) != "s",
+      o1[English == FALSE & Capitals == TRUE & nchar(Words) > 3 & substrRight(Words, 1) != "s",
          .(count = .N),
          .(Company, Words)][order(-count)]
 
     print(o2)
 
+    # combine results with final data.table
     if (exists("fin_o") == FALSE)
       fin_o <- o2
     else
@@ -140,36 +143,35 @@ for (i in file_names[2002:2867]) {
   })
 }
 
+# Group output by Company and Words, then sum the counts
 fin_o <- fin_o[, .(tot_cnt=sum(count)),.(Company, Words)][order(Company, -tot_cnt)]
 
+# Write to file
 write_csv(fin_o, "data/business_innovation/working/word_counts_2001_2867.csv")
 
-#write.csv(example2, file = "data/business_innovation/working/dn_table.csv")
-
-# stringr::str_locate_all(cleaned, "<DOCUMENT><TYPE>...*?")
-# substr(unclean, 2342566-10, 2342566+1500)
-
-
+# Combine files
 f1 <- fread("data/business_innovation/working/word_counts_1_1000.csv")
 f2 <- fread("data/business_innovation/working/word_counts_1001_2000.csv")
 f3 <- fread("data/business_innovation/working/word_counts_2001_2867.csv")
 f_all <- rbindlist(list(f1,f2,f3))
+# Group by Company and Words, then sum all tot_cnts
 f_all <- f_all[, .(tot_cnt=sum(tot_cnt)),.(Company, Words)][order(Company, -tot_cnt)]
+# Write combined to a file
 write_csv(f_all, "data/business_innovation/working/word_counts_all.csv")
 
 
 
-
-#CIK Names
+# Add company names to combined file
+f_all_2 <- fread("data/business_innovation/working/word_counts_all.csv")
 master_index <- readRDS("~/git/business_innovation/data/business_innovation/original/master_index.RDS")
+# Get unique CIK and Company Names
 cik_unique <- unique(master_index[, .(CIK, COMPANY_NAME)])
-
+# Merge with combined file
 f_all_names <- merge(f_all_2, cik_unique, by = "CIK", all.x = TRUE)
 
+# Group by CIK, Words, and tot_cnt, then collapse multiple names for a CIK into a comma delimited list
 f_all_names_cat <- f_all_names[, .(company_name = paste(COMPANY_NAME, collapse = ", ")), .(CIK, Words, tot_cnt)]
+# Write combined file with names to a file
 write_csv(f_all_names_cat, "data/business_innovation/working/word_counts_w_cat_names_all.csv")
 
 
-
-####
-#file_names <- unique(list.files("data/business_innovation/original/edgar_filings/Edgar_filings_folders", full.names = TRUE))
